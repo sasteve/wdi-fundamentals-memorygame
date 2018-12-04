b4780b70f2bb1655bd182702244240f6f4f912df
sasteve <steve_oldani@yahoo.com>
2018-12-04 06:52:43 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,24 +3,24 @@ console.log("Up and running!");
 
 var cards = [
 {
-	rank: "queen",
-	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage:'images/queen-of-hearts.png'
 },
 {
-	rank: "queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
 },
 {
-	rank: "king",
-	suit: "hearts",
-	cardImage: "images/king-of-hearts.png"
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
 },
 {
-	ranks: "king",
-	suit: "diamonds",
-	cardImage: "images/king-of-diamonds.png"
+	ranks: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
 }
 ];
 
@@ -28,25 +28,34 @@ var cardsInPlay = [];
 
 var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+	alert ("You found a match!");
     } else {
-   	alert("Sorry, try again.");
+   	alert ("Sorry, try again.");
     }
 };
+var flipCard = function () {
+	cardId = this.getAttribute('data-id');
+	console.log(cardId);
 
-var flipCard = function (cardId) {
-	var cardId = (cards[cardId].rank);
-	console.log("User fliped " + cards[cardId].rank);
-	console.log(cards[cardId].suit);
-	console.log(cards[cardId].cardImage);
-	cardsInPlay.push(cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].card);
+	this.setAttribute('src', cards[cardId].cardImage);
+
     if (cardsInPlay.length === 2) {
     	checkForMatch();
     }
-  
-flipCard(0);
-flipCard(1);
 };
+  
+  var createBoard = function() {
+  	for (var i = 0; i < cards.length; i++) {
+  		var cardElement = document.createElement('img');
+  		cardElement.setAttribute('src', 'images/back.png');
+  		cardElement.setAttribute('data-id', i);
+  		cardElement.addEventListener('click', flipCard);
+  		document.getElementById('game-board').appendChild(cardElement);
+  	}
+  }
+createBoard();
+
 
 
 
